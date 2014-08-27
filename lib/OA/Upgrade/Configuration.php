52b374bfd30cd96fc875c121390215264089d260
Matteo Beccati <matteo@beccati.com>
2014-08-27 12:24:14 +0200

    Cleaned up a few more &s

--- lib/OA/Upgrade/Configuration.php
+++ lib/OA/Upgrade/Configuration.php
@@ -23,7 +23,7 @@ class OA_Upgrade_Config
     function OA_Upgrade_Config()
     {
         $this->oSettings = new OA_Admin_Settings();
-        $this->aConfig =& $this->oSettings->getConfigArray();
+        $this->aConfig = $this->oSettings->getConfigArray();
         // set default configPath
         $this->configPath = MAX_PATH.'/var/';
         if (!OA_Admin_Settings::isConfigWritable())
